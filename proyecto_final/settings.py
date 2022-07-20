9af514fcb09b4969789fd6bcfa6b4037b8be6631
Lucas Dowhyj <dowhyjlucas@hotmail.com>
2022-07-20 13:18:14 -0300

    Modifico index

--- proyecto_final/settings.py
+++ proyecto_final/settings.py
@@ -17,6 +17,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_DIR = os.path.join(BASE_DIR, "static")
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -120,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [STATIC_DIR,]
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
